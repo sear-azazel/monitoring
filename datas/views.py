94e4faec020bb5f85cf235429fb7f70115a2f55f
sear-azazel <sear.azazel@gmail.com>
2019-06-27 10:31:19 +0900

    fix: 最新20件を表示するようにする

--- datas/views.py
+++ datas/views.py
@@ -6,5 +6,12 @@ from .models import Recognition
 class RecognitionListView(ListView):
 
     model = Recognition
-    paginate_by = 20
     template_name = 'recognition_list.html'
+
+    def get_queryset(self, queryset=None):
+        return Recognition.objects.order_by('pub_date').reverse()[:20]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
